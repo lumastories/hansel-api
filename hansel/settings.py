ca2058b578d557f707ded01ec9de39b5d497b6c1
Nate Nichols <natsamnic@gmail.com>
2016-10-04 19:28:57 -0700

    set PRODUCTION=False in api/ignored.py

--- hansel/settings.py
+++ hansel/settings.py
@@ -64,16 +64,24 @@ WSGI_APPLICATION = 'hansel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'hansel',
-        'USER': 'hansel',
-        'PASSWORD': DB_PASSWORD,
-        'HOST': 'localhost',
-        'PORT': '',
+if PRODUCTION:
+    DATABASES = { 
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'hansel',
+            'USER': 'hansel',
+            'PASSWORD': DB_PASSWORD,
+            'HOST': 'localhost',
+            'PORT': '',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
